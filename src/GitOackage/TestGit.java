d1ce6985f0e9cb2abe37543fa32c3385a4019af7
Aarti <aartibisen1011@gmail.com>
2022-10-08 20:13:02 +0530

    new project

---
+++ src/GitOackage/TestGit.java
@@ -0,0 +1,22 @@
+package GitOackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class TestGit {
+	
+		
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		//System.out.println("Hello Java");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Downloads\\chromedriver.exe");
+		//C:\Users\Dell\Downloads
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Downloads\\chromedriver_win32\\chromedriver.exe");
+				WebDriver driver=new ChromeDriver();
+				driver.get("https://chromedriver.chromium.org/downloads");
+				//driver.close();
+				
+			}
+
+		}
+
+
+
